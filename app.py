99860c2f97f884ec347d5b4782e2240cf7103ead
Cdisi1999 <christiandisi1999@gmail.com>
2024-11-21 16:47:48 -0500

    Fifth Submission

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
+df = pd.read_csv('vehicles_us.csv')
+
+st.write("Dataset Loaded Successfully:")
+st.write(df)
+
+st.header("Vehicle Data Analysis Dashboard")
+
+fig_hist = px.histogram(df, x='price', title='Price Distribution', log=True)
+st.plotly_chart(fig_hist)
